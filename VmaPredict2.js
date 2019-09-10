f1291e1564aca4d54f36c8767c16cf417881ba37
PGTBoos <23136027+PGTBoos@users.noreply.github.com>
2019-09-10 15:39:56 +0200

    updated comments

--- VmaPredict2.js
+++ VmaPredict2.js
@@ -1,8 +1,20 @@
-// worked great 30 min candle polinex 11 month chart  74% above market  (extreme high)
-// so this math has potentiall, however misses a lot chances too
-// profit was unstable in other testdata  (must not be test data dependable so much)
+// VmaPredict2.js By Peter Boos  CC BY    2019
+// source of this (and maybe updated) code : https://github.com/PGTBoos/GekkoStrategies
+//
+// This code is easy to adapt and alter please play with it,
+// You can improve the code or play with the settings
+// Adding new rules (in the main at check if).
+// I used a bit fancy naming in the static variables.
+// In the code they have a prefix of Swing thus they becomme this.SwingBullsEye
+// your free to use and alter this code, trading though is what you do at your own risk 
+// If you got something good tweeked or altered share it on the gekko forums.
+// https://forum.gekko.wizb.it
+
+
+
 
 var strat = {
+    // those variables are available to all functions and adressed with this. like this.Tick
     Gotcoins: false,
     BoughtAt: 0,
     MaxPeekClose: 0,
@@ -21,6 +33,7 @@ var strat = {
     SwingMaxPeekPred:0,
     SwingBullsEye:0,
 
+    //Color array is only used for visual console logging.
     //https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
     color: {
         Reset: '\x1b[0m',
@@ -48,7 +61,7 @@ var strat = {
         
     },
 
-
+    //Startup setting initial variables reading the setttings from the Toml file
     init: function () {
         this.SwingMagica      =  this.settings.Swings.Magica; 
         this.SwingMediumOut   =  this.settings.Swings.MediumOut;
@@ -57,7 +70,7 @@ var strat = {
         this.SwingBullsEye    =  this.settings.Swings.SwingBullsEye;
         // getting the toml variables in
 
-      console.log(this.settings);
+     
      
         this.addTulipIndicator('timeseriesforcastLong', 'tsf',  { optInTimePeriod: this.settings.ForcastTrend.Long }); // statiscal forcasting });
         this.addTulipIndicator('timeseriesforcastMedium', 'tsf',{ optInTimePeriod: this.settings.ForcastTrend.Medium });
@@ -68,10 +81,12 @@ var strat = {
         this.addTulipIndicator('vmaMedium', 'vwma',             { optInTimePeriod: this.settings.VolumePeriodAverage.Medium });  //trade volume weighted averages
         this.addTulipIndicator('vmaShort', 'vwma',              { optInTimePeriod: this.settings.VolumePeriodAverage.Short });
         this.addTulipIndicator('vmaLong', 'vwma',               { optInTimePeriod: this.settings.VolumePeriodAverage.Long });
-
+       
+        console.log(this.settings);
      
     },
 
+    // update triggers before check (here i set peeks and set this.Beartrends)
     update: function (candle) {
         this.CurrentCandle = candle;
         if (candle.close > this.MaxPeekClose){this.MaxPeekClose=candle.close};
@@ -81,40 +96,49 @@ var strat = {
     },
 
     check: function (candle) {
+        // reading out the tulip indicators and set those in local variables of the check function
+        
         let ind = this.tulipIndicators,
             predictLong = ind.timeseriesforcastLong.result.result,
             predictMedium = ind.timeseriesforcastMedium.result.result,
-            predictShort = ind.timeseriesforcastShort.result.result,
+            predictShort = ind.timeseriesforcastShort.result.result,     //Time Series Forecast short 
             maShort = ind.maShort.result.result,
             maMedium = ind.maMedium.result.result,
-            maLong = ind.maLong.result.result,
+            maLong = ind.maLong.result.result,                           //Moving Average Long
             vmaShort = ind.vmaShort.result.result,
             vmaMedium = ind.vmaMedium.result.result,
-            vmaLong = ind.vmaLong.result.result;
+            vmaLong = ind.vmaLong.result.result;                         //Volume moving Average Long
 
-            
+        // here the trading happens
 
 
-
-        if (!this.Gotcoins) {
-            if (vmaMedium > maMedium && maShort < maMedium && maLong<predictMedium) {                   this.long('Uptopica     ', this.color.Green); }
-            else if(candle.close+this.SwingMagica <predictMedium &&maMedium<maLong){              this.long('Magica       ', this.color.White);}
-           // else if(mashort >predictShort+){}
+        if (!this.Gotcoins) { //decide a buy moment
+            if (vmaMedium > maMedium && maShort < maMedium && maLong<predictMedium) {   this.long('Uptopica     ', this.color.Green); }
+            else if(candle.close+this.SwingMagica <predictMedium &&maMedium<maLong){    this.long('Magica       ', this.color.White);}
+   
         }
-        else {                               // > predicmedium
-            if (vmaMedium < maMedium && maShort > maMedium+this.SwingMediumOut) { this.short('MediumOut    ', this.color.Magenta); }
-            else if(maShort +this.SwingShortSight <predictShort ) {               this.short('ShortSight   ', this.color.Yellow);}
-        //    else if(this.BoughtAt >candle.close+250) {this.short('peekout     ',this.color.Yellow)}
-           else if (this.BoughtAt+this.SwingBullsEye <candle.close)                             this.short('BullsEye     ',this.color.Blue);
-          // else if (this.LongBeartrend && candle.close >this.BoughtAt*0.1)          this.short('BearsEye     ',this.color.White);
-           else if(this.MaxPeekClose >predictMedium+this.SwingMaxPeekPred) {      this.short('MaxpeekPred  ',this.color.Cyan);};
+        else {                //decide a sell moment
+            if (vmaMedium < maMedium && maShort > maMedium+this.SwingMediumOut) {       this.short('MediumOut    ', this.color.Magenta); }
+            else if(maShort +this.SwingShortSight <predictShort ) {                     this.short('ShortSight   ', this.color.Yellow);}
+  
+           else if (this.BoughtAt+this.SwingBullsEye <candle.close)                     this.short('BullsEye     ',this.color.Blue);
+       
+           else if(this.MaxPeekClose >predictMedium+this.SwingMaxPeekPred) {            this.short('MaxpeekPred  ',this.color.Cyan);};
         };
+       
+        // if you want to log variables you can do use console.log 
         //console.log(candle.close,predictLong, predictMedium, predictShort);
   
  
     },
 
 
+
+
+// At the moment of going Short or Long some internal variables change.
+// For example this.Gotcoins changes
+// Also in regard to colors a background other then black is a buy
+// Bear market moments are red, bull markets (when market goes upwards) are green.
 
 
 short: function (msg, kleur = this.color.Default) {
@@ -130,7 +154,7 @@ short: function (msg, kleur = this.color.Default) {
             trend = this.color.Green + 'Bear ';
         }
         else {
-            this.BullProffit = this.BullProffit + profit;
+            this.BullProffit = this.BullProffit + profit; // see at the end remarks.
             trend = this.color.Red + 'Bull ';
         }
         console.log(trend + kleur + msg + prof);
@@ -160,6 +184,10 @@ long: function (msg, kleur = this.color.Default) {
 
 },
 end: function () {
+
+    // this is not a real profit but handy for checking math against bull and bear markets.
+    // this could be extended for example to show the gains per triggered rule
+    // I might create something alike that later
     console.log('Bear profit', this.BearProffit, 'Bull proffit', this.BullProffit);
 }
 
